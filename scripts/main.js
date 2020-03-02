ac549a37c6eb57951dc2453301353aa93ad56bf5
Zakaria Moslim <35297640+ZakMos@users.noreply.github.com>
2020-03-02 12:30:19 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -205,7 +205,7 @@
             } else if(userAnswer.toLowerCase() === questions[current].answer.toLowerCase()){
                 this.state.score++;
                 this.view.printResult('Correct! You might not be as dumb as I thought you were.');
-            } else if (userAnswer.value.length === ""){
+            } else if (userAnswer.value.length == 0){
                 alert('This field is requierd, try again!');
             } else if(userAnswer.value.length >= 1) {
                 this.view.printResult('Wrong human, try again!');
